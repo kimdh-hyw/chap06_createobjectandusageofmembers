785d6adc9c318298a9e9f50b92db7860e8aaabe4
kimdh-hyw <71587564+kimdh-hyw@users.noreply.github.com>
2020-09-25 21:49:48 +0900

    first upload version 1.0.0 (SubPackage Name Modified)

---
+++ src/sec01/createobjectandusageofmembers/EX01_CreateObjectAndUsageOfMembers/CreateObjectAndUsageOfMembers.java
@@ -0,0 +1,27 @@
+package sec01.createobjectandusageofmembers.EX01_CreateObjectAndUsageOfMembers;
+
+
+//#1. 클래스의 구성 (정의) : 붕어빵 기계
+class A {
+	int m=3;
+	
+	void print() {
+		System.out.println("객체 생성 및 활용");
+	}
+}
+
+public class CreateObjectAndUsageOfMembers {
+	public static void main(String[] args) {
+		
+		//#2. 클래스로 부터 객체 생성 : 붕어빵
+		A a = new A();
+		
+		//#3. 클래스 멤버 활용 (필드활용:필드에 값을 읽기/쓰기, 메서드를 활용: 메서드를 호출): 붕어빵을 먹기 
+		a.m=5;
+		System.out.println(a.m); //5
+		
+		a.print(); //객체 생성 및 활용
+		
+		
+	}
+}
